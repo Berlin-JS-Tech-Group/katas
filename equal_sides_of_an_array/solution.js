b3f23c69b0c0612e11dc6440a7d857e4e4695d3b
Batyr Kanzitdinov <dev@batyr.io>
2022-11-22 21:41:52 +0100

    formatting

--- equal_sides_of_an_array/solution.js
+++ equal_sides_of_an_array/solution.js
@@ -1,20 +1,17 @@
-export function findEvenIndex({ input })
-{
+export function findEvenIndex({ input }) {
   const arr = input;
 
-  const arrSum = (_arr) => _arr.reduce(
-    (sum, currentValue) => sum + currentValue,
-    0
-  );
+  const arrSum = (_arr) =>
+    _arr.reduce((sum, currentValue) => sum + currentValue, 0);
 
   for (const [index, it] of arr.entries()) {
     const arrL = arr.slice(0, index);
     const arrR = arr.slice(index + 1);
-    
+
     if (arrSum(arrL) === arrSum(arrR)) {
       return index;
     }
   }
-  
+
   return -1;
 }
